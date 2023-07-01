4630ebe68c30c3860f936d3f8627d515a3c8439e
qwq <qwq@qwq.cat>
2023-07-01 11:45:20 +0800

    style: prettier format

--- test/index.test.ts
+++ test/index.test.ts
@@ -1,7 +1,7 @@
-import { expect, it } from 'vitest'
-import { foo } from '../src'
+import { expect, it } from 'vitest';
+import { foo } from '../src';
 
 it('Should be defined', () => {
-  expect(foo).toBeDefined()
-  expect(foo()).toBe('foo')
-})
+  expect(foo).toBeDefined();
+  expect(foo()).toBe('foo');
+});
