202eb94627828ebc7fbdc8d46f950be8639f56a3
qwq <qwq@qwq.cat>
2023-08-11 11:45:42 +0800

    feat: add submodules exports

--- tsup.config.ts
+++ tsup.config.ts
@@ -3,7 +3,7 @@ import { defineConfig } from 'tsup';
 export default defineConfig({
   clean: true,
   dts: { resolve: true },
-  entry: ['src/index.ts'],
+  entry: ['src/*.ts'],
   format: ['cjs', 'esm'],
   shims: true,
 });
